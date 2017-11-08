cddbaf10d13369a83b9b67aaf02be67e9879af67
Dimpu Aravind Buddha <buddhaaravind@gmail.com>
2017-11-08 18:51:03 -0500

    workd

--- src/store/basic-field-type/selectors.js
+++ src/store/basic-field-type/selectors.js
@@ -8,7 +8,12 @@ import {
 const basicFieldTypes = state => state.basicFieldTypes;
 const selectedFormId = state => state.app.get('selectedFormId');
 
-const getBasicFieldTypes = createSelector(basicFieldTypes, (basicFieldTypes) => basicFieldTypes.map(field => field.setIn(['json','formId'], selectedFormId)));
+const getBasicFieldTypes = createSelector(
+    [basicFieldTypes, selectedFormId],
+    (basicFieldTypes, selectedFormId) => 
+    basicFieldTypes.map(
+        field => field.setIn(['json', 'formId'], selectedFormId)
+    ));
 
 
 export default {
